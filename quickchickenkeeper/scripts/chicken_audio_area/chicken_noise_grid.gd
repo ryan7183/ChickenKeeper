68c3cf039394653979f7b56f6c966f004d8cef5c
Ryan Seguin <ryanseguin@hotmail.ca>
2025-05-28 16:55:11 -0400

    Threaded chicken sounds

--- quickchickenkeeper/scripts/chicken_audio_area/chicken_noise_grid.gd
+++ quickchickenkeeper/scripts/chicken_audio_area/chicken_noise_grid.gd
@@ -6,45 +6,95 @@ var chicken_audio_area_preload:PackedScene = preload("res://scenes/chicken_audio
 
 var grid_size:Vector2 =Vector2(1600, 1600)
 var num_audio_players_per_row:int  = 10
-var _new_chicken_positions:PackedVector2Array = []
 var _current_chicken_positions:PackedVector2Array = []
 var areas:Array[ChickenAudioArea] = []
 
+var areas_positions:Array[Vector2] = []
+var areas_positions_mutex:Mutex = Mutex.new()
+var max_distance:float = 2000
+var max_distance_mutex:Mutex = Mutex.new()
+var chicken_counts:Array[int] = []
+var chicken_counts_mutex:Mutex = Mutex.new()
 var chicken_count_thread:Thread = Thread.new()
+var start_count:Semaphore = Semaphore.new()
+var _current_chicken_positions_mutex:Mutex = Mutex.new()
+var exit_count_thread:bool = false
+var exit_count_thread_mutex:Mutex = Mutex.new()
+var new_chicken_counts:bool =false
+var new_chicken_counts_mutex:Mutex =Mutex.new()
+
+func _ready() -> void:
+	
+	pass
 
 func _process(delta: float) -> void:
-	_calculate_num_players_in_areas()
+	new_chicken_counts_mutex.lock()
+	if new_chicken_counts:
+		new_chicken_counts = false
+		chicken_counts_mutex.lock()
+		set_num_players_in_areas(chicken_counts)
+		chicken_counts_mutex.unlock()
+	new_chicken_counts_mutex.unlock()
+
+func _exit_tree() -> void:
+	
+	# End thread on exit
+	exit_count_thread_mutex.lock()
+	exit_count_thread = true
+	exit_count_thread_mutex.unlock()
+	start_count.post()
+	chicken_count_thread.wait_to_finish()
+	pass
 
 func _contunually_count_chickens()->void:
+	
+	while true:
+		start_count.wait()
+		
+		exit_count_thread_mutex.lock()
+		if exit_count_thread:
+			break
+		exit_count_thread_mutex.unlock()
+		
+		_calculate_num_players_in_areas()
+		pass
 	pass
 
 func _calculate_num_players_in_areas()->void:
-	_current_chicken_positions = _new_chicken_positions
+	_current_chicken_positions_mutex.lock()
+	
 	var counts:Array[int] = []
 	counts.resize(areas.size())
 	counts.fill(0)
+	areas_positions_mutex.lock()
+	max_distance_mutex.lock()
 	for chicken:Vector2 in _current_chicken_positions:
 		for area_index:int in range(areas.size()):
 			var area:ChickenAudioArea = areas[area_index]
-			if chicken.distance_to(area.position)<= area.max_distance:
+			if chicken.distance_to(areas_positions[area_index])<= max_distance:
 				counts[area_index] += 1
 				break
 			pass
 		pass
-	set_num_players_in_areas(counts)
-	pass
+	areas_positions_mutex.unlock()
+	max_distance_mutex.unlock()
+	_current_chicken_positions_mutex.unlock()
+	new_chicken_counts_mutex.lock()
+	new_chicken_counts = true
+	chicken_counts = counts
+	new_chicken_counts_mutex.unlock()
 
-func set_num_players_in_areas(chicken_counts:Array[int])->void:
+func set_num_players_in_areas(counts:Array[int])->void:
 	for i:int in range(areas.size()):
-		if chicken_counts[i] < 5:
+		if counts[i] < 5:
 			areas[i].set_num_players(0)
-		elif chicken_counts[i]<10:
+		elif counts[i]<10:
 			areas[i].set_num_players(1)
-		elif chicken_counts[i]<50:
+		elif counts[i]<50:
 			areas[i].set_num_players(2)
-		elif chicken_counts[i]<250:
+		elif counts[i]<250:
 			areas[i].set_num_players(3)
-		elif chicken_counts[i]<750:
+		elif counts[i]<750:
 			areas[i].set_num_players(4)
 		else:
 			areas[i].set_num_players(5)
@@ -52,22 +102,32 @@ func set_num_players_in_areas(chicken_counts:Array[int])->void:
 func setup()->void:
 	clear_audio_areas_children()
 	var spacing:float = grid_size.x/num_audio_players_per_row
+	max_distance = spacing * 1.5
 	var starting_pos:Vector2 = Vector2(spacing/2, spacing/2)
+	areas_positions_mutex.lock()
+	max_distance_mutex.lock()
 	for x:int in range(num_audio_players_per_row):
 		for y:int in range(int(grid_size.y/spacing)):
 			var audio_pos:Vector2 = starting_pos + Vector2(spacing*x, spacing*y)
 			var audio_area:ChickenAudioArea = chicken_audio_area_preload.instantiate()
 			audio_area.position = audio_pos
-			audio_area.max_distance = spacing *1.1
+			audio_area.max_distance = max_distance
 			areas.append(audio_area)
+			areas_positions.append(Vector2(audio_pos.x, audio_pos.y))
 			audio_areas.add_child(audio_area)
 			#audio_area.set_num_players(5)
 			pass 
 		pass
+	areas_positions_mutex.unlock()
+	max_distance_mutex.unlock()
+	chicken_count_thread.start(_contunually_count_chickens)
 	pass
 
 func set_chicken_positions(positions:PackedVector2Array)->void:
-	_new_chicken_positions = positions
+	if _current_chicken_positions_mutex.try_lock():
+		_current_chicken_positions = positions
+		_current_chicken_positions_mutex.unlock()
+		start_count.post()
 	pass
 
 func clear_audio_areas_children()->void:
