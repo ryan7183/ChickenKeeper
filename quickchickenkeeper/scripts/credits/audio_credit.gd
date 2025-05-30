3819f95efdd6328fd2dc9b55a13c26f056ac51bd
Ryan Seguin <ryanseguin@hotmail.ca>
2025-05-29 22:37:07 -0400

    Credits now have links

--- quickchickenkeeper/scripts/credits/audio_credit.gd
+++ quickchickenkeeper/scripts/credits/audio_credit.gd
@@ -6,7 +6,7 @@ class_name AudioCredit extends Control
 @export_multiline var author:String
 @export_multiline var url:String
 @export_multiline var license:String
-
+@export_multiline var license_url:String
 
 @export var asset_title_text:RichTextLabel
 @export var author_text:RichTextLabel
@@ -18,12 +18,19 @@ class_name AudioCredit extends Control
 func _ready() -> void:
 	asset_title_text.text = asset_title
 	author_text.text = author
-	url_text.text = url
-	license_text.text = license
+	url_text.text = '[url={"url": "'+url+'"}]'+url+'[/url]'
+	license_text.text = '[url={"url": "'+license_url+'"}]'+license+'[/url]'
 	audio_stream_player.stream = audio_file
 	pass
 
 
 func _on_texture_button_pressed() -> void:
 	audio_stream_player.playing = !audio_stream_player.playing 
+	pass # Replace with function body.
+
+func _on_url_meta_clicked(meta: Variant) -> void:
+	var data:Dictionary = JSON.parse_string(meta)
+	if data.has("url"):
+		var site:String = data["url"]
+		OS.shell_open(site)
 	pass # Replace with function body.
--- quickchickenkeeper/scripts/credits/image_credit.gd
+++ quickchickenkeeper/scripts/credits/image_credit.gd
@@ -6,7 +6,7 @@ class_name ImageCredit extends Control
 @export_multiline var author:String
 @export_multiline var url:String
 @export_multiline var license:String
-
+@export_multiline var license_url:String
 
 #@export var credit_label:Label
 @export var asset_title_text:RichTextLabel
@@ -19,7 +19,15 @@ class_name ImageCredit extends Control
 func _ready() -> void:
 	asset_title_text.text = asset_title
 	author_text.text = author
-	url_text.text = url
-	license_text.text = license
+	url_text.text = '[url={"url": "'+url+'"}]'+url+'[/url]'
+	license_text.text = '[url={"url": "'+license_url+'"}]'+license+'[/url]'
 	image_container.texture = image
 	pass
+
+
+func _on_url_meta_clicked(meta: Variant) -> void:
+	var data:Dictionary = JSON.parse_string(meta)
+	if data.has("url"):
+		var site:String = data["url"]
+		OS.shell_open(site)
+	pass # Replace with function body.
