3e7abc9ebf505ee562613d0512d9d10dcf67af1f
Ryan Seguin <ryanseguin@hotmail.ca>
2025-06-01 13:58:18 -0400

    Added Godot credit

--- quickchickenkeeper/scripts/credits/credits.gd
+++ quickchickenkeeper/scripts/credits/credits.gd
@@ -5,3 +5,9 @@ signal back_button_pressed
 func _on_button_pressed() -> void:
 	back_button_pressed.emit()
 	pass # Replace with function body.
+
+
+func _on_texture_rect_pressed() -> void:
+	var site:String = "https://godotengine.org/license/"
+	OS.shell_open(site)
+	pass # Replace with function body.
