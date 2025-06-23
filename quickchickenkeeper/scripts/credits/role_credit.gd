ec90b6738c6d06def8a03af424e21f70b8378c52
Ryan Seguin <ryanseguin@hotmail.ca>
2025-06-23 18:44:07 -0400

    Linked to website in credits

--- quickchickenkeeper/scripts/credits/role_credit.gd
+++ quickchickenkeeper/scripts/credits/role_credit.gd
@@ -2,8 +2,16 @@
 class_name RoleCredit extends Control
 
 @export_multiline var credit_text:String
-@export var credit_label:Label
+@export var credit_label:RichTextLabel
+@export_multiline var link_url:String
 
 func _ready() -> void:
-	credit_label.text = credit_text
+	credit_label.text = '[url={"url": "'+link_url+'"}]'+credit_text+'[/url]'
 	pass
+
+func _on_label_meta_clicked(meta: Variant) -> void:
+	var data:Dictionary = JSON.parse_string(meta)
+	if data.has("url"):
+		var site:String = data["url"]
+		OS.shell_open(site)
+	pass # Replace with function body.
